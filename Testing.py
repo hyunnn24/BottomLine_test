c863fe9503667424ed79704c6cd380fe090bf3b9
hyunnn24 <snow3515@naver.com>
2024-05-30 03:32:51 +0000

    ㄴ이ㅏㅓ라ㅓㅁㄴ이러ㅣㄴ어힌어히ㅓㄴㅁㅇㄹ

--- Testing.py
+++ Testing.py
@@ -1,3 +1,23 @@
 import streamlit as st
+from openai import OpenAI
 
-print("Hello World")
+
+def APIINPUT():
+    st.header("API Key 를 입력하세요")
+    API = st.text_input("API", type="password")
+    if API:
+        st.session_state.API = API
+
+def page2():
+
+def page3():
+
+page = st.sidebar.selectbox("페이지 선택", ["API", "페이지2", "페이지3"])
+
+
+if page == "API":
+    APIINPUT()
+elif page == "페이지2":
+    page2()
+elif page == "페이지3":
+    page3()
