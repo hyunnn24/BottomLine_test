f88c06da89fb35b74171a26264ae4640340c93d0
hyunnn24 <snow3515@naver.com>
2024-06-13 08:34:29 +0000

    페이지 재편

--- Testing.py
+++ Testing.py
@@ -1,23 +1,22 @@
 import streamlit as st
 
-# 페이지 레이아웃 설정
-col1, col2 = st.columns(2)
+# 사이드바에 선픽과 후픽 선택 상자 추가
+st.sidebar.header("선픽")
+pre_pick_option = st.sidebar.selectbox('선픽할 항목을 선택하세요:', ['항목 1', '항목 2', '항목 3'], key='pre_pick')
+st.sidebar.write(f'선택된 선픽 항목: {pre_pick_option}')
 
-# 선픽 페이지
-with col1:
-    st.header("선픽 페이지")
-    st.write("이곳은 선픽 페이지입니다.")
-    # 선픽과 관련된 기능 추가
-    option = st.selectbox('선픽할 항목을 선택하세요:', ['항목 1', '항목 2', '항목 3'], key='pre_pick')
-    st.write(f'선택된 항목: {option}')
+st.sidebar.header("후픽")
+post_pick_option = st.sidebar.selectbox('후픽할 항목을 선택하세요:', ['항목 A', '항목 B', '항목 C'], key='post_pick')
+st.sidebar.write(f'선택된 후픽 항목: {post_pick_option}')
 
-# 후픽 페이지
-with col2:
-    st.header("후픽 페이지")
-    st.write("이곳은 후픽 페이지입니다.")
-    # 후픽과 관련된 기능 추가
-    option = st.selectbox('후픽할 항목을 선택하세요:', ['항목 A', '항목 B', '항목 C'], key='post_pick')
-    st.write(f'선택된 항목: {option}')
+# 메인 페이지에 선택된 값 표시
+st.title("선픽 & 후픽 페이지")
+
+st.header("선픽")
+st.write(f'선택된 선픽 항목: {pre_pick_option}')
+
+st.header("후픽")
+st.write(f'선택된 후픽 항목: {post_pick_option}')
 
 # 공통 기능 추가 가능
 st.write("공통 기능이나 추가 정보를 이곳에 넣을 수 있습니다.")
