db7d6b3e2c33b2601ebbdf51f60a697358f08b44
hyunnn24 <snow3515@naver.com>
2024-06-13 08:35:19 +0000

    ektldl

--- Testing.py
+++ Testing.py
@@ -1,22 +1,21 @@
 import streamlit as st
 
-# 사이드바에 선픽과 후픽 선택 상자 추가
-st.sidebar.header("선픽")
-pre_pick_option = st.sidebar.selectbox('선픽할 항목을 선택하세요:', ['항목 1', '항목 2', '항목 3'], key='pre_pick')
-st.sidebar.write(f'선택된 선픽 항목: {pre_pick_option}')
+# 페이지 선택
+page = st.sidebar.selectbox("페이지 선택", ["선픽"],["후픽"])
 
-st.sidebar.header("후픽")
-post_pick_option = st.sidebar.selectbox('후픽할 항목을 선택하세요:', ['항목 A', '항목 B', '항목 C'], key='post_pick')
-st.sidebar.write(f'선택된 후픽 항목: {post_pick_option}')
+if page == "선픽":
+    st.title("선픽 페이지")
+    st.write("이곳은 선픽 페이지입니다.")
+    # 선픽과 관련된 기능 추가
+    option = st.selectbox('선픽할 항목을 선택하세요:', ['항목 1', '항목 2', '항목 3'])
+    st.write(f'선택된 항목: {option}')
 
-# 메인 페이지에 선택된 값 표시
-st.title("선픽 & 후픽 페이지")
-
-st.header("선픽")
-st.write(f'선택된 선픽 항목: {pre_pick_option}')
-
-st.header("후픽")
-st.write(f'선택된 후픽 항목: {post_pick_option}')
+elif page == "후픽":
+    st.title("후픽 페이지")
+    st.write("이곳은 후픽 페이지입니다.")
+    # 후픽과 관련된 기능 추가
+    option = st.selectbox('후픽할 항목을 선택하세요:', ['항목 A', '항목 B', '항목 C'])
+    st.write(f'선택된 항목: {option}')
 
 # 공통 기능 추가 가능
 st.write("공통 기능이나 추가 정보를 이곳에 넣을 수 있습니다.")
